d1e663efa632a9ff093747eb16e1cc46e0bc00e9
Cedric Perauer <40869163+Cedmalloc@users.noreply.github.com>
2019-02-10 16:31:54 +0100

    Add files via upload

---
+++ Complex_Numbers/5.26_complex_numbers/include/Complex.h
@@ -0,0 +1,34 @@
+#ifndef COMPLEX_H_
+#define COMPLEX_H_
+
+#include <iostream>
+using namespace std;
+
+namespace C {
+
+class Complex {
+private:
+	double real;
+	double imag;
+
+public:
+	Complex();
+	Complex(double real, double imag);
+	Complex(const Complex &other);
+	const Complex &operator=(const Complex &other);
+
+	double getReal() const {
+		return real;
+	}
+	double getImag() const {
+		return imag;
+	}
+};
+//
+ostream &operator<<(ostream &out, const Complex &c);
+Complex operator*(const Complex &c1);
+Complex operator+(const Complex &c1, const Complex &c2);
+
+} /* namespace caveofprogramming */
+
+#endif /* COMPLEX_H_ */
