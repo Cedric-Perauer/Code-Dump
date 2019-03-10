9e5ab00a7a6d14f49a9b8147d56994d522851bf5
Cedric Perauer <40869163+Cedric-Perauer@users.noreply.github.com>
2019-03-11 00:54:00 +0100

    Update University_of_Santa_Cruz_Course_1.cpp

--- University_of_Santa_Cruz_Course_1.cpp
+++ University_of_Santa_Cruz_Course_1.cpp
@@ -1,57 +1,21 @@
-#include <iostream>
-
-/* Convert this program to C++
-
-* change to C++ io
-
-* change to one line comments
-
-* change defines of constants to const
-
-* change array to vector<>
-
-* inline any short function
-
-*/
-
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
 const int N =  40;
 
-inline void sum(int*p, int n, vector<int> d)
-
-{
-
-    int i;
-
-    *p = 0;
-
-    for(i = 0; i < n; ++i)
-
-        *p = *p + d[i];
-
+inline void sum(int*p, int n, vector<int> d) {
+    int i; *p = 0;
+    for(i = 0; i < n; ++i) {*p = *p + d[i];}
 }
 
 int main()
 
-{
-
-    int i;
-
-    int accum = 0;
-
-    vector<int> data;
-
-    for(i = 0; i < N; ++i)
-        
-        data.push_back(i);
-
-    sum(&accum, N, data);
-
-    printf("sum is %d\n", accum);
-
-    return 0;
+{    int i,accum = 0;
+     vector<int> data;
+     for(i = 0; i < N; ++i)  {data.push_back(i);}
+     sum(&accum, N, data);
+     cout << "sum is :" << accum << endl;
+     return 0;
 
 }
